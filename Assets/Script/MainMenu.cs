543a6f653884e4f5bb0c163e458e9e0667d435b6
MrBearA <110599403+MrBearA@users.noreply.github.com>
2025-04-02 00:28:46 +0800

    mainmenu updated

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
     }
 
     public void Quit()
