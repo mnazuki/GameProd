4575964cecbdac247c44c90ced00d48d9cd7e860
ariiezona <115564021+ariiezona@users.noreply.github.com>
2025-04-12 03:00:26 +0800

    fixed anabolism pausing issue

--- Assets/Script/AnabolismMinigame1/GameManager.cs
+++ Assets/Script/AnabolismMinigame1/GameManager.cs
@@ -14,14 +14,16 @@ public class AnabolismGameManager : MonoBehaviour
                 hintScreenOpen = false;
                 hintScreen.SetActive(false);
                 Debug.Log("Tab pressed. Hint screen closed.");
-                Debug.Log("Pausing, timescale = 0");
+                Debug.Log("Resuming, timescale = 1");
+                Time.timeScale = 1f;
             }
             else
             {
                 hintScreenOpen = true;
                 hintScreen.SetActive(true);
                 Debug.Log("Tab pressed. Hint screen opened.");
-                Debug.Log("Resuming, timescale = 1");
+                Debug.Log("Pausing, timescale = 0");
+                Time.timeScale = 0f;
             }
         }
     }
