f0af943fad836dd5007c03a56474b230355d96f7
AnamXI <anamggss1227@gmail.com>
2025-04-12 07:08:18 +0800

    habol update

--- Assets/Script/UI/GuideSheet.cs
+++ Assets/Script/UI/GuideSheet.cs
@@ -6,9 +6,12 @@ public class GuideSheet : MonoBehaviour
     [SerializeField] private GameObject guideSheet;
     private bool guideToggle = false;
 
+    [SerializeField] private AudioSource[] src;
+
     // Update is called once per frame
     void Start(){
         Time.timeScale = 1f; // To prevent bugs
+        src = GameObject.Find("SFX")?.GetComponents<AudioSource>();
     }
     
     void Update()
@@ -30,6 +33,7 @@ public class GuideSheet : MonoBehaviour
 
                 Time.timeScale = 0f;
                 guideToggle = true;
+                src[1].Play();
             }
     }
 
@@ -39,6 +43,7 @@ public class GuideSheet : MonoBehaviour
 
                 Time.timeScale = 1f;
                 guideToggle = false;
+                src[1].Play();
             }
 
     }
